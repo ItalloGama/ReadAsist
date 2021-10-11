afae05a8ea2ef4e78ed5409a78ef82f39b1674a9
ItalloGama <Itallo.gama15@gmail.com>
2021-10-11 12:40:25 -0400

    users model

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// routes go here
+
+
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/api', AppRouter)
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
