60e6a4e36248a75b9bf44484ad4cdb244594a937
ItalloGama <Itallo.gama15@gmail.com>
2021-10-11 16:27:49 -0400

    3 tip seeds made

--- seeders/20211011194038-book.js
+++ seeders/20211011194038-book.js
@@ -2,15 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert(
+      'books',
+      [
+        {
+          description: 'Harry Potter',
+          img: 'other url here',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20211011194048-tip.js
+++ seeders/20211011194048-tip.js
@@ -2,15 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert(
+      'tips',
+      [
+        {
+          tipName: 'Build an App.',
+          tip: 'write better',
+          img: 'cool url',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
