86719de16cedaf348f30142250d9d2072471fffc
ItalloGama <Itallo.gama15@gmail.com>
2021-10-11 13:25:27 -0400

    initial migrations name changed to lower case

--- migrations/20211011170542-create-note.js
+++ migrations/20211011170542-create-note.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Notes', {
+    await queryInterface.createTable('notes', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Notes');
+    await queryInterface.dropTable('notes');
   }
 };
